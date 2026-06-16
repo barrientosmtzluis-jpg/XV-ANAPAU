24d490713da07e0c2b087d799c3d649748fc4c5e
barrientosmtzluis-jpg <barrientosmtz.luis@gmail.com>
2026-06-15 20:31:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,60 @@ function tick(){
  document.getElementById('countdown').innerHTML=`${days} días ${h} horas ${m} minutos ${s} segundos`;
 }
 setInterval(tick,1000);tick();
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.carousel-track img');
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    track.style.transform =
+        `translateX(-${index * 100}%)`;
+
+}
+
+document.querySelector('.next')
+.addEventListener('click',()=>{
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    showSlide(currentSlide);
+
+});
+
+document.querySelector('.prev')
+.addEventListener('click',()=>{
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+
+        currentSlide = slides.length - 1;
+
+    }
+
+    showSlide(currentSlide);
+
+});
+
+/* Cambio automático cada 5 segundos */
+
+setInterval(()=>{
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    showSlide(currentSlide);
+
+},5000);
